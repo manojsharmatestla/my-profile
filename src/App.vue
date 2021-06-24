af20e2ecbf1cbb6d01155840112bf93093d96331
manojsharmatestla <manoj.sharma@mercedes-benz.io>
2021-06-24 21:08:57 +0200

    Profile

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="justify-center flex bg-blue-400 items-center h-screen">
+    <div class="text-4xl">
+      profile
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
