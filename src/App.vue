64afebcdaae5ee6013733c1a9350f5c45aaf4a3e
manojsharmatestla <manoj.sharma@mercedes-benz.io>
2021-06-25 01:17:33 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class="flex bg-blue-400 h-screen">
-    <div class="text-4xl flex-row  w-11/12">
-      <div class="bg-gray-900 "> 
-          Testing
+  <div class="justify-center flex bg-blue-400 h-screen">
+    <div class="text-4xl flex-row  w-3/4 py-20">
+      <div class="bg-gray-900 rounded-lg p-10">
+        <nav>
+          <div class="flex text-lg">
+            <div class="flex-col flex-auto justify-items-start">
+              <p class="text-white justify-items-start mx-0 w-60 font-bold text-2xl"> <span class="text-2xl text-yellow-400">M</span>anoj Sharma</p>
+            </div>
+            <div class="flex-col">
+              <ul class="flex justify-items-end text-white">
+                <li class="px-5">about me</li>
+                <li class="px-5">profile</li>
+                <li class="px-5">resume</li>
+                <li class="px-5">contact</li>
+              </ul>
+            </div>
+            
+          </div>
+        </nav>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-  }
+  name: "App",
+  components: {},
 });
 </script>
 
